c238ee3a601d0517a777b9b90a80c65d77668813
Omer Mishania <66549913+omermishania@users.noreply.github.com>
2022-04-02 15:38:52 +0300

    Configured the deafult .tf file
    removed the second worker group
    removed user data metadata
    changed desired amount to 1
    changed cluster version to k8s 1.21

--- aws/eks-cluster.tf
+++ aws/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.20"
+  cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
@@ -14,16 +14,8 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
+      instance_type                 = "t2.tiny"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
   ]
