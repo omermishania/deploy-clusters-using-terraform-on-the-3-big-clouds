522255d1597f9da62d56c5510bc0f8cef67abc2e
Omer Mishania <66549913+omermishania@users.noreply.github.com>
2022-05-01 19:52:18 +0300

    Update eks-cluster.tf

--- aws/eks-cluster.tf
+++ aws/eks-cluster.tf
@@ -3,19 +3,12 @@ module "eks" {
   version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
-  subnets         = module.vpc.private_subnets
-
   vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
 
   worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.micro"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 1
     },
   ]
